7b3cbff6d12562d2b754eed409cd482557990423
simeunji <ssimeunji@wrapcore.co.kr>
2023-06-13 16:05:04 +0900

    img test

--- push.js
+++ push.js
@@ -7,6 +7,6 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:ssimeunji24@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dpLX21ClZZs:APA91bHOeWn8l_58nzq1eymD71-dCbzXSuWPmUkOy_mgBEDxQyrjqkLkxA9FNV7afVoEtv5SIsfgSeetYl-EqIHlokM-NEjSb0mBj5Z7bNQcGZ-4DT-3MjYUceYnheBjVzlfruofFgC0","expirationTime":null,"keys":{"p256dh":"BP2n3msuwyUl1efMl5DB1kkza4No8AWPK-1LvKPmzrbY9bqKfx2_yz-AAqj-h64peQKOZ2yeOehWv3oGzCUR5OY","auth":"aFHKccLseLZYFWwI6ck0XQ"}};
 
 push.sendNotification(sub, 'test message');
